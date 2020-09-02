29c8ba141f7fa478bdd6f51a12aee72521efa339
Fake <44167242+Lebensborn@users.noreply.github.com>
2020-09-02 09:08:05 +0800

    更新 服务器.cpp

--- 计网课设/服务器.cpp
+++ 计网课设/服务器.cpp
@@ -4,43 +4,24 @@
 #include<stdlib.h> 
 #include<string> 
 #include<assert.h> 
-#include<iostream>
+//#include<iostream>
 #include<conio.h>
 #include<fstream>
 #include<thread>
 #include<vector>
 #include"查询域名.h"
 
-using namespace std;
+//using namespace std;
 #define SIZE 128 //定义用于接收数据的数组的大小
-int usernum = 0;//当前注册用户的人数
+//int usernum = 0;//当前注册用户的人数
+//extern dns_servers[];
+//extern void ngethostbyname(unsigned char*, int, int);
 
-class user {
-public:
-	int num;//用户序号
-	int clifd;//socket标识
-	void operator()();//用于创建对应用户的线程
-	void getmsg(char* a);//从客户端接收信息并存入字符串中
-	void sendmsg(char* buf);//发送信息
-};
-user alluser[20];
-
-void user::operator()() {
-	int answer;
-	char question[SIZE] = { "请输入要查询的域名\n" };	
-	char domain[SIZE];
-	while (1) {
-		sendmsg(question);		
-		getmsg(domain);
-		//由域名获得IPv4地址，A是查询类型
-		ngethostbyname((unsigned char*)domain, A,clifd);
-	}	
-}
-void user::sendmsg(char* buf) {
+void sendmsg(char* buf,int clifd) {
 	send(clifd, buf, strlen(buf), 0);
 }
 
-void user::getmsg(char* a) {
+void getmsg(char* a,int clifd) {
 	//char buff[SIZE] = { '\0' };
 	int n = recv(clifd, a, SIZE - 1, 0);
 	if (n <= 0)
@@ -52,6 +33,24 @@ void user::getmsg(char* a) {
 	printf("%d : %s\n", clifd, a);
 	send(clifd, "\nOK\n", strlen("\nOK\n"), 0);
 }
+
+DWORD WINAPI comm_client(LPVOID lpParamter) //子进程用于处理和客户端交互
+
+{
+	int* a = (int*)lpParamter;
+	int clifd = a[0];
+	int answer;
+	char question[SIZE] = { "请输入要查询的域名\n" };
+	char domain[SIZE];
+	while (1) {
+		sendmsg(question,clifd);
+		getmsg(domain,clifd);
+		//由域名获得IPv4地址，A是查询类型
+		ngethostbyname((unsigned char*)domain, 1, clifd);
+	}
+	return 0L;
+}
+
 
 int main(int argc, char* argv[])
 
@@ -68,7 +67,7 @@ int main(int argc, char* argv[])
 	struct sockaddr_in ser;
 	ser.sin_family = AF_INET;
 	ser.sin_port = htons(6000);
-	ser.sin_addr.s_addr = inet_addr("192.168.3.8");
+	ser.sin_addr.s_addr = inet_addr("10.128.193.151");
 	int res = 0;
 	res = bind(listenfd, (struct sockaddr*) & ser, sizeof(ser));
 	assert(-1 != res);
@@ -83,17 +82,18 @@ int main(int argc, char* argv[])
 		struct sockaddr_in cli;
 		int len = sizeof(cli);
 		int clifd = accept(listenfd, (struct sockaddr*) & cli, &len);
-		cout << "accept" << endl;
+		//cout << "accept" << endl;
 		if (clifd < 0)
 		{
 			printf("link error.\n");
 			continue;
 		}
-		alluser[usernum].clifd = clifd;
-		alluser[usernum].num = usernum;
-		thread my_thread(alluser[usernum]);
-		my_thread.detach();
-		usernum++;
+		//alluser[usernum].clifd = clifd;
+		//alluser[usernum].num = usernum;
+		//thread my_thread(alluser[usernum]);
+		//my_thread.detach();
+		CreateThread(NULL, 0, comm_client, &clifd, 0, NULL);
+		//usernum++;
 	}
 	closesocket(listenfd);
 	return 0;
